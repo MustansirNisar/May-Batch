a0c16494cc5c523ecd22a524980e8bae2addd327
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-02 19:58:55 +0530

    Add files via upload

---
+++ JavaScript/varibles.js
@@ -0,0 +1,22 @@
+// let rollNumber2 = 44;
+// console.log(rollNumber2)
+
+// let fullName = "Mustansir"
+// let age222;
+// let $price;
+
+// let 2age;
+
+// let Name = "Mustansir"
+// let name = "Samar"
+
+// console.log(name)
+// console.log(Name)
+
+// const let = 5;
+
+// let function = 22;
+    
+
+
+
