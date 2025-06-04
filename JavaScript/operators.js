a3370bc1ea2c04e3bbebadc9410d1cff398390f6
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-04 22:24:12 +0530

    Add files via upload

---
+++ JavaScript/operators.js
@@ -0,0 +1,75 @@
+// let a = 10;
+// let b = 3;
+// let sum = a + b;
+
+// console.log("The sum of a and b is:", sum); //Addition
+// console.log(a - b); //Subtraction
+// console.log(a * b) // Multiplication
+// console.log(a / b); // Division
+// console.log(a % b); //Modulus
+// console.log(a ** b) //Exponentiation
+// let x = 5;
+// x++; //Addition of 1
+// console.log(x)
+// let x = 6;
+// x--; //Subtraction of 1
+// console.log(x)
+
+
+// let y = 10;
+// console.log(y++); //prints or ouputs 10, then increments y to become 11
+// console.log(++y)
+
+
+// let y = 6;
+// console.log(y--);
+// console.log(--y)
+
+// let x = 10;
+ // x = x + 5
+// console.log(x += 5)
+// console.log(x)
+// console.log(x -= 2);
+// console.log(x *= 2);
+// console.log(x)
+
+// let a = 10;
+// let b = 10;
+// console.log(a == b)
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a <= b)
+// console.log(a != b)
+
+// let a = 5;
+// let b = "5"; //this string is converted to a number
+// console.log(a == b); //loose comparison //value is considered
+// console.log(a === b); //Strict comparison // vlaue + data type is considered
+
+//!= !==
+
+let age = 25;
+let marks = 90;
+// console.log(age<18 && marks>100)
+// console.log(age<18 || marks>100)
+// console.log(!age<18)
+// console.log(!marks>100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
