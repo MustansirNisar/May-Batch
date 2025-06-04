1942bb3223d36623aa68fa77911f091343c7e5f4
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-04 22:25:18 +0530

    Add files via upload

---
+++ JavaScript/datatypes.js
@@ -0,0 +1,37 @@
+// let fullName = "Mustansir"
+// console.log(typeof(fullName))
+// let age = "30";
+// console.log(typeof(age))
+
+// let age = 25;
+// console.log(typeof(age))
+// let price = 220.4;
+// console.log(typeof(price))
+
+// let isLoggedIn = true;
+// let isLoggedOut = false;
+// console.log(typeof(isLoggedIn))
+// console.log(typeof(isLoggedOut))
+
+// let user;
+// console.log(typeof(user))
+
+// let user = null;
+// console.log(typeof(user))
+
+// let bigNumber = 10030879889032479598909878676089786n;
+// let x = BigInt("9807657680997")
+// console.log(typeof(bigNumber))
+
+
+// let id = Symbol("id")
+// console.log(typeof(id))
+
+//int a = 19 // Statically typed
+
+// let x = 19;
+// let x = "Mustansir" // Dynamically typed
+// let x = true;
+
+
+
