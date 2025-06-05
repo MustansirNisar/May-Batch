fac906ef56782a8b2637b785bf3faecdfa23b01b
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-05 19:26:50 +0530

    Update conditionals.js

--- JavaScript/conditionals.js
+++ JavaScript/conditionals.js
@@ -22,27 +22,27 @@
      code = "S24"
      console.log("Your code is: ", code);
  }
-// let age = 17;
-// if(age>=18){
-//     console.log("you can vote");
-// }
-// else{
-//     console.log("You can't vote")
-// }
+ let age = 17;
+ if(age>=18){
+     console.log("you can vote");
+ }
+ else{
+     console.log("You can't vote")
+ }
 
-// let score = 80;
-// if(score>=90){
-//     console.log("Grade: A")
-// }
-// else if(score >=80){
-//     console.log("Grade: B")
-// }
-// else if(score>=70){
-//     console.log("Grade: C")
-// }
-// else{
-//     console.log("You didn't qualify")
-// }
+ let score = 80;
+ if(score>=90){
+     console.log("Grade: A")
+ }
+ else if(score >=80){
+     console.log("Grade: B")
+ }
+ else if(score>=70){
+     console.log("Grade: C")
+ }
+ else{
+     console.log("You didn't qualify")
+ }
 
 // condition?value_if_true:value_if_false;
 // let age = 17;
