3178af11043a05b56736e912cd8e190bc26762cc
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-05 19:23:21 +0530

    Add files via upload

---
+++ JavaScript/conditionals.js
@@ -0,0 +1,57 @@
+// let age = 20;
+// if (age>=18){
+//     console.log("You are eligible to vote");
+// }
+// let age = 15;
+// if(age>=18){
+//     console.log("You are eligible to vote")
+// }
+// if(age<18){
+//     console.log("You are not eligible to vote")
+// }
+
+// console.log("Okay")
+
+// let fullName = "Samar"
+// let code;
+// if(fullName==="Safal"){
+//     code = "S23"
+//     console.log("Your code is: ", code);
+// }
+// if(fullName==="Samar"){
+//     code = "S24"
+//     console.log("Your code is: ", code);
+// }
+// let age = 17;
+// if(age>=18){
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("You can't vote")
+// }
+
+// let score = 80;
+// if(score>=90){
+//     console.log("Grade: A")
+// }
+// else if(score >=80){
+//     console.log("Grade: B")
+// }
+// else if(score>=70){
+//     console.log("Grade: C")
+// }
+// else{
+//     console.log("You didn't qualify")
+// }
+
+// condition?value_if_true:value_if_false;
+// let age = 17;
+// age>18?console.log("vote"):console.log("Not Vote")
+
+// let num = Number(prompt("Enter any number: "));
+
+// console.log("Entered number:", num);
+// console.log(typeof(num));
+
+
+
