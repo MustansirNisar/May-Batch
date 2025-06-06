3bb75caa039dbf8bdf71a877e94c8cf34996ed33
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-06 11:56:53 +0530

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,22 +1,22 @@
-// var age; //Declaration
-// age = 25; //Initialization
+ var age; //Declaration
+ age = 25; //Initialization
 
-// var age = 25; //Declaration + Initialization
-
-
-// var age = 28;
-// var age = 30;
-// console.log(age)
-// age = 30;
-// console.log(age)
+ var age = 25; //Declaration + Initialization
 
 
-// let age = 29;
-// let age = 30;
-// age = 40;
-// console.log(age)
+ var age = 28;
+ var age = 30;
+ console.log(age)
+ age = 30;
+ console.log(age)
 
-// const age = 22;
-// const age = 40;
-// age = 50;
-// console.log(age)
+
+ let age = 29;
+ let age = 30;
+ age = 40;
+ console.log(age)
+
+ const age = 22;
+ const age = 40;
+ age = 50;
+ console.log(age)
