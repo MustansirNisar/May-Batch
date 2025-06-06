dba1faf8ff9cc51bcfc2d4dbf2465ccf727fbc2c
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-06 11:45:09 +0530

    Add files via upload

---
+++ JavaScript/loops.js
@@ -0,0 +1,90 @@
+// for(let i=0; i<10; i++ ){
+//     console.log("The value of i is: ", i);
+// }
+
+//Calculate the sum of first ten numbers
+// let sum = 0;
+// for(let i=1; i<=10; i++){
+//     sum = sum + i;
+// }
+// console.log("The sum of the first ten numbers is: ", sum);
+
+
+// let i=0;
+// while(i<10){
+//     console.log("The value of i is: ", i)
+//     i++;
+
+// }
+// let i=12;
+// do{
+//     console.log("Hello and Welcome");
+//     i++;
+// }while(i<10);
+
+
+
+// let message = "Welcome"
+// for(let char of message ){
+//     console.log(char)
+// }
+// let size = 0;
+// let fullName = "Mustansir"
+// for(let letter of fullName){
+//     console.log(letter)
+//     size++;
+// }
+// console.log("The number of chars in the name are:", size)
+
+
+let student = {
+    fullName: "Safal",
+    id: 10,
+    age: 22,
+    cgpa: 9.9,
+    address:{
+        add1: "NY",
+        add2: "Nepal"
+        },
+    course: "full stack development",
+    isPass: true
+}
+for(let key in student){
+    console.log(key, ":", student[key])
+}
+// console.log(student)
+// console.log(student.fullName)
+// console.log(student.isPass)
+// console.log(student.cgpa)
+// console.log(student["course"])
+
+// student.city = "NY"
+// console.log(student.address.add1)
+
+// console.log(student)
+// student.cgpa = 10
+// console.log(student)
+
+// delete student.id
+// console.log(student)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
