e70dae30b258c42bb12cb02164f00a08707441e4
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-08 10:23:19 +0530

    Update strings.js

--- JavaScript/strings.js
+++ JavaScript/strings.js
@@ -3,9 +3,9 @@ let fullName = '      PraTiksha     '
  console.log(fullName[5])
  console.log(fullName[0])
 
-// let newstring = fullName.toUpperCase();
-// fullName.toLowerCase();
-// console.log(fullName)
-// console.log(newstring)
-console.log(fullName)
+ let newstring = fullName.toUpperCase();
+ fullName.toLowerCase();
+ console.log(fullName)
+ console.log(newstring)
+ console.log(fullName)
 let nw = console.log(fullName.trim())
