bd35c3bda05aa08d0f0b7e2216e9cee5321bf89b
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-08 10:22:31 +0530

    Add files via upload

---
+++ JavaScript/strings.js
@@ -0,0 +1,11 @@
+let fullName = '      PraTiksha     '
+// console.log(fullName.length)
+// console.log(fullName[5])
+// console.log(fullName[0])
+
+// let newstring = fullName.toUpperCase();
+// fullName.toLowerCase();
+// console.log(fullName)
+// console.log(newstring)
+console.log(fullName)
+let nw = console.log(fullName.trim())
