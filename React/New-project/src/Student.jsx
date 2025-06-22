86d892e037428f510ee0e97dcd9bb8e09e50b4b6
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-22 21:10:38 +0530

    Add files via upload

---
+++ React/New-project/src/Student.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Course from './Course'
+
+function Student() {
+  return (
+    <div style={{backgroundColor: "violet", border: "2px solid black", padding: "10PX"}}>
+      <h1>Student Component</h1>
+      <Course/>
+    </div>
+  )
+}
+
+export default Student
