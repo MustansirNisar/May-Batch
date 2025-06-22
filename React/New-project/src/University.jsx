55ec15d0d39fd73d70fabf98b1263946a591a6d9
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-22 21:11:06 +0530

    Add files via upload

---
+++ React/New-project/src/University.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Depart from './Depart'
+
+function University() {
+  return (
+    <div style={{backgroundColor: "aquamarine", border: "2px solid black", padding: "10PX"}}>
+      <h1>University Component</h1>
+      <Depart/>
+    </div>
+  )
+}
+
+export default University
