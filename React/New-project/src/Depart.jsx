aa53345d058ef029006c290d13a641e0abee67ef
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-23 22:40:49 +0530

    Add files via upload

---
+++ React/New-project/src/Depart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Student from './Student'
+
+function Depart() {
+  return (
+    <div style={{backgroundColor: "lightGreen", border: "2px solid black", padding: "10PX"}}>
+      <h1>Department Component</h1>
+      <Student/>
+    </div>
+  )
+}
+
+export default Depart
