a9d8db2cf34c37645adcdd079955d722407f7cf7
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-06-23 22:41:22 +0530

    Add files via upload

---
+++ React/New-project/src/DataContext.jsx
@@ -0,0 +1,15 @@
+import React, { createContext } from 'react'
+
+export let courseContext = createContext("DBMS");
+
+
+
+function DataContext() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DataContext
